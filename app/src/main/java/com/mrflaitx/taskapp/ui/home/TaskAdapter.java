f8785db48793535098d00a5023c868958761aa21
phenomenalMatrix <mrflaitx@gmail.com>
2021-12-06 21:06:19 +0600

    init commit

---
+++ app/src/main/java/com/mrflaitx/taskapp/ui/home/TaskAdapter.java
@@ -0,0 +1,50 @@
+package com.mrflaitx.taskapp.ui.home;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.mrflaitx.taskapp.databinding.ItemRvBinding;
+
+import java.util.ArrayList;
+
+public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.ViewHolder> {
+
+    private ArrayList<String> list = new ArrayList<>();
+    private ItemRvBinding binding;
+
+    public void setText(String text){
+        list.add(text);
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        binding = ItemRvBinding.inflate(LayoutInflater.from(parent.getContext()),parent,false);
+        return new ViewHolder(binding.getRoot());
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.onBind(list.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+
+        public void onBind(String s) {
+            binding.titleRv.setText(s);
+        }
+    }
+}
